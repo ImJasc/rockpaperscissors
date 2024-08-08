80e33b5156d9783762df8b63781f238975ddd54e
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-08 12:49:48 -0700

    created the function to play a round

--- scrypt.js
+++ scrypt.js
@@ -25,3 +25,44 @@ function getHumanChoice(){
 
 let humanScore = 0, computerScore = 0;
 
+function playRound (humanC, ComputerC){       
+    switch (humanC.toLowerCase()){
+        case "paper":
+            if (ComputerC == 'Rock'){
+                console.log("You Win! Paper beats Rock.");
+                humanScore++;
+            }else if (ComputerC == 'Paper'){
+                console.log("It's a Tie! Both players choosed Paper.");
+            }else{
+                console.log("You lose! Scissors beats Paper.");
+                computerScore++;
+            }
+        break;
+        case "scissors":
+            if (ComputerC == "Paper"){
+                console.log("You Win! Scissors beats Paper.");
+                humanScore++
+            }else if (ComputerC == "Scissors"){
+                console.log("It's a Tie! Both players choosed Scissors.");
+            }else{
+                console.log("You lose! Rock beats Paper.");
+                computerScore++;
+            }
+        break;
+        case "rock":
+            if (ComputerC == "Scissors"){
+                console.log("You Win! Rock beats Scissors.");
+                humanC++;
+            }else if (ComputerC == "Rock"){
+                console.log("It's a Tie! Both players choosed Rock.");
+            }else{
+                console.log("You lose! Paper beats Rock.");
+                computerScore++;
+            }
+        break;  
+        default:
+            console.log("Either you misspelled the word or your choose wasn't an option. Round skipped.")                        
+            break;                      
+    }
+}
+
